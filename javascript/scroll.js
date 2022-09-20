ab4186b6f41e1150c1a2450127abfab39a37be19
wagnerwg0202 <97996373+wagnerwg0202@users.noreply.github.com>
2022-09-19 22:05:15 -0300

    update animation scroll

--- javascript/scroll.js
+++ javascript/scroll.js
@@ -1,5 +1,6 @@
 //logica da animaçao do scroll
 
+//otimizar as funcoes ativadas pelo scroll
 const debounce = function(func, wait, immediate) {
   let timeout;
   return function(...args) {
@@ -15,6 +16,8 @@ const debounce = function(func, wait, immediate) {
   };
 };
 
+
+//animaçao nas imagens
 const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
@@ -34,5 +37,28 @@ animeScroll();
 if(target.length) {
   window.addEventListener('scroll', debounce(function() {
     animeScroll();
+  }, 200));
+}
+
+//animacão no texto
+const targettext = document.querySelectorAll('[data-anime-text]');
+const animationtextClass = 'animatetext';
+
+function animetextScroll() {
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+  targettext.forEach(function(element) {
+    if((windowTop) > element.offsetTop) {
+      element.classList.add(animationtextClass);
+    } else {
+      element.classList.remove(animationtextClass);
+    }
+  })
+}
+
+animetextScroll();
+
+if(target.length) {
+  window.addEventListener('scroll', debounce(function() {
+    animetextScroll();
   }, 200));
 }
